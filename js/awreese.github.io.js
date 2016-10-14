dc4514588cb2f9c956a457cd9439cbb942b8d4ad
Drew Reese <drew.w.reese@gmail.com>
2016-10-14 10:36:28 -0700

    terminal on page should now completely load aboutMe from file

--- js/awreese.github.io.js
+++ js/awreese.github.io.js
@@ -5,55 +5,36 @@
 
     // loads webpage controls
     window.onload = function () {
-        // load controls
-        var terminalText = document.querySelector("terminal-text");
 
-        // openFile();
+        loadText();
+
+        // $(document).ready(function(){
+        //     $.get("aboutMe.txt", null, function(data, status) {
+        //         $("#terminal-text").empty();
+
+        //         var lines = data.split('\n');
+
+        //         for (var i = 0; i < lines.length; i++) {
+        //             $("#terminal-text").append("<p>" + lines[i] + "</p>");
+        //         }
+
+        //     }, "text");
+        // });
+    };
+
+    function loadText() {
         $(document).ready(function(){
-            // $("#terminal-text").load("aboutMe.txt");
-            // $("#terminal-text").text("loaded");
             $.get("aboutMe.txt", null, function(data, status) {
-                // $("#terminal-text").text(data);
                 $("#terminal-text").empty();
 
                 var lines = data.split('\n');
-                // var aboutMeText = "";
 
                 for (var i = 0; i < lines.length; i++) {
-                    // aboutMeText += "<p>" + lines[i] + "</p>";
                     $("#terminal-text").append("<p>" + lines[i] + "</p>");
                 }
 
-                // $("#terminal-text").html(aboutMeText);
             }, "text");
         });
-    };
-
-    /*
-    Ajax helper function.
-    Parameters:
-    functionName - function to call when response loads
-    request - XMLHttpRequest being made
-    */
-    function ajaxRequest(functionName, request) {
-        var ajax = new XMLHttpRequest();
-        ajax.onload = functionName;
-        ajax.open("GET", request, true);
-        ajax.send();
     }
-
-    function openFile() {
-        ajaxRequest(processFile, ABOUTME);
-    }
-
-    function processFile() {
-        debugEvent("processFile()", this);
-
-        if (this.status === 200) {
-            document.querySelector(terminalText).innerHTML = this.responseText;
-        }
-    }
-
-    
 
 })();
