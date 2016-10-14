f6cc82b1767db51bd38f989ec78897914e3cc58d
Drew Reese <drew.w.reese@gmail.com>
2016-10-14 09:14:52 -0700

    Adding script to load aboutMe.txt

---
+++ js/awreese.github.io.js
@@ -0,0 +1,49 @@
+(function () {
+    "use strict";
+
+    var ABOUTME = "aboutMe.txt";
+
+    // loads webpage controls
+    window.onload = function () {
+        // load controls
+        var terminalText = document.querySelector("terminal-text");
+
+        // openFile();
+        $("#terminal-text").text("loaded");
+        $(document).ready(function(){
+            // $("#terminal-text").load("aboutMe.txt");
+            // $("#terminal-text").text("loaded");
+            $.get("aboutMe.txt", null, function(data, status) {
+                alert("data: " + data);
+            }, "text");
+        });
+    };
+
+    /*
+    Ajax helper function.
+    Parameters:
+    functionName - function to call when response loads
+    request - XMLHttpRequest being made
+    */
+    function ajaxRequest(functionName, request) {
+        var ajax = new XMLHttpRequest();
+        ajax.onload = functionName;
+        ajax.open("GET", request, true);
+        ajax.send();
+    }
+
+    function openFile() {
+        ajaxRequest(processFile, ABOUTME);
+    }
+
+    function processFile() {
+        debugEvent("processFile()", this);
+
+        if (this.status === 200) {
+            document.querySelector(terminalText).innerHTML = this.responseText;
+        }
+    }
+
+    
+
+})();
