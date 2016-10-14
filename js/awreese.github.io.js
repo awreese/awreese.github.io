afe01ad9f60c03f85193881b805bbc33fa631093
Drew Reese <drew.w.reese@gmail.com>
2016-10-14 14:16:52 -0700

    trying forEach loop to consume text file

--- js/awreese.github.io.js
+++ js/awreese.github.io.js
@@ -1,40 +1,70 @@
 (function () {
     "use strict";
 
-    var ABOUTME = "aboutMe.txt";
+    var ABOUTME      = "aboutMe.txt";
+    var TERMINAL     = ".terminal";
+    
+    var PROMPT       = "t-prompt";
+    var PULSATE      = "pulsate";
+    var PROMPTSTRING = "drew@github:~$ ";
 
-    // loads webpage controls
+    // loads webpage functions
     window.onload = function () {
 
-        loadText();
+        loadTerminal(ABOUTME);
 
-        // $(document).ready(function(){
-        //     $.get("aboutMe.txt", null, function(data, status) {
-        //         $("#terminal-text").empty();
-
-        //         var lines = data.split('\n');
-
-        //         for (var i = 0; i < lines.length; i++) {
-        //             $("#terminal-text").append("<p>" + lines[i] + "</p>");
-        //         }
-
-        //     }, "text");
-        // });
     };
 
-    function loadText() {
-        $(document).ready(function(){
-            $.get("aboutMe.txt", null, function(data, status) {
-                $("#terminal-text").empty();
+    function loadTerminal(dataFile) {
+        console.log("cat " + dataFile);
 
-                var lines = data.split('\n');
+        $(TERMINAL).empty();
+        $(TERMINAL).append(new prompt("cat " + dataFile));
 
-                for (var i = 0; i < lines.length; i++) {
-                    $("#terminal-text").append("<p>" + lines[i] + "</p>");
-                }
+        $(document).ready(function() {
+            $.get(dataFile, null, function(data) {
 
-            }, "text");
+                // var paragraphs = data.split('\n');
+                
+                // for (var i = 0; i < paragraphs.length; i++) {
+                //     console.log(paragraphs[i]);
+                //     $(TERMINAL).append(new paragraph(paragraphs[i]));
+                // }
+
+                data.split('\n').forEach(function(p) {
+                    console.log(p);
+                    $(TERMINAL).append(new paragraph(p));
+                });
+
+            });
         });
+
+        $(TERMINAL).append(new prompt());
+    }
+
+    function prompt(command) {
+        var $prompt = $('<p></p>');
+        $prompt.addClass(PROMPT);
+        if (command) {
+            $prompt.text(PROMPTSTRING + command);
+        } else {
+            $prompt.text(PROMPTSTRING);
+            $prompt.append(new cursor());
+        }
+        return $prompt;
+    }
+
+    function cursor() {
+        var $cursor = $('<span></span>');
+        $cursor.addClass(PULSATE);
+        $cursor.text('_');
+        return $cursor;
+    }
+
+    function paragraph(text) {
+        var $paragraph = $('<p></p>');
+        $paragraph.text(text);
+        return $paragraph;
     }
 
 })();
