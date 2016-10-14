42df7a7709b845dd679f51e6cd10386b574d938d
Drew Reese <drew.w.reese@gmail.com>
2016-10-14 15:49:20 -0700

    added documentation, cleaned up comments, organized vars

--- js/awreese.github.io.js
+++ js/awreese.github.io.js
@@ -1,71 +1,70 @@
+/*
+Copyright 2016 Andrew Reese, All rights reserved.
+Licensed under GNU GPL (https://www.gnu.org/licenses/licenses.html).
+*/
+
 (function () {
     "use strict";
 
-    var ABOUTME      = "aboutMe.txt";
-    var TERMINAL     = ".terminal";
+    var ABOUTME       = "aboutMe.txt";
+    var TERMINAL      = ".terminal";
+    var CAT           = "cat ";
     
-    var PROMPT       = "t-prompt";
-    var PULSATE      = "pulsate";
-    var PROMPTSTRING = "drew@github:~$ ";
+    var PROMPT_CLASS  = "t-prompt";
+    var PULSATE_CLASS = "pulsate";
+
+    var PROMPT_STRING  = "drew@github:~$ ";
 
     // loads webpage functions
     window.onload = function () {
 
-        // loadTerminal(ABOUTME);
-        $.get(ABOUTME, loadTerminal(ABOUTME), "text"); // Asynch call!
+        $.get(ABOUTME, loadAboutMe(TERMINAL, CAT + ABOUTME), "text"); // Asynch call!
 
     };
 
-    function loadTerminal(dataFile) {
+    /*
+    Loads specified terminal with about me file output.
+    */
+    function loadAboutMe(terminal, command) {
 
         return function(data) {
-            console.log("cat " + dataFile);
+            console.log(command);
 
-            $(TERMINAL).empty();
-            $(TERMINAL).append(prompt("cat " + dataFile));
+            $(terminal).empty();
+            $(terminal).append(new prompt(command));
 
-            // $(document).ready(function() {
-                // $.get(dataFile, function(data) {
+            data.split('\n').forEach(function(p) {
+                console.log(p);
+                $(terminal).append(new paragraph(p));
+            });
 
-                    // var paragraphs = this.split('\n');
-                    
-                    // for (var i = 0; i < paragraphs.length; i++) {
-                    //     console.log(paragraphs[i]);
-                    //     $(TERMINAL).append(paragraph(paragraphs[i]));
-                    // }
-
-                    data.split('\n').forEach(function(p) {
-                        console.log(p);
-                        $(TERMINAL).append(new paragraph(p));
-                    });
-
-                // });
-            // });
-
-            $(TERMINAL).append(prompt());
+            $(terminal).append(new prompt());
         }
 
     }
 
+    // Returns BASH prompt with specified command, or cursor if not provided
     function prompt(command) {
-        var $prompt = $('<p></p>');
-        $prompt.addClass(PROMPT);
+        var $prompt = new paragraph();
+        $prompt.addClass(PROMPT_CLASS);
         if (command) {
             $prompt.text(PROMPTSTRING + command);
         } else {
             $prompt.text(PROMPTSTRING);
-            $prompt.append(cursor());
+            $prompt.append(new cursor());
         }
         return $prompt;
     }
 
+    // Returns pulsing cursor for BASH prompt
     function cursor() {
         var $cursor = $('<span></span>');
-        $cursor.addClass(PULSATE);
+        $cursor.addClass(PULSATE_CLASS);
         $cursor.text('_');
         return $cursor;
     }
 
+    // Returns paragraph html element containing specified text
     function paragraph(text) {
         var $paragraph = $('<p></p>');
         $paragraph.text(text);
